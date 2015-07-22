e5e8c010be3b3767ecb9eb9c26c76a259c6b6bb0
GrahamTheCoder <grahamthecoder@gmail.com>
2015-07-22 15:29:22 +0100

    Create a solution for building in .NET 3.5

--- mcs/class/Mono.Security/Assembly/AssemblyInfo.cs
+++ mcs/class/Mono.Security/Assembly/AssemblyInfo.cs
@@ -40,7 +40,7 @@ using System.Runtime.InteropServices;
 
 // General Information about the system assembly
 
-[assembly: AssemblyVersion (Consts.FxVersion)]
+[assembly: AssemblyVersion ("1.0.0.0")]
 
 [assembly: AssemblyCompany ("MONO development team")]
 [assembly: AssemblyCopyright ("(c) 2003-2004 Various Authors")]
@@ -56,9 +56,6 @@ using System.Runtime.InteropServices;
 // incomplete - prevent testing on Windows
 //[assembly:SecurityPermission (SecurityAction.RequestOptional, UnmanagedCode=true)]
 
-
-[assembly: AssemblyDelaySign (true)]
-[assembly: AssemblyKeyFile ("../mono.pub")]
 
 #if MOBILE
 [assembly: InternalsVisibleTo ("System, PublicKey=00240000048000009400000006020000002400005253413100040000010001008D56C76F9E8649383049F383C44BE0EC204181822A6C31CF5EB7EF486944D032188EA1D3920763712CCB12D75FB77E9811149E6148E5D32FBAAB37611C1878DDC19E20EF135D0CB2CFF2BFEC3D115810C3D9069638FE4BE215DBF795861920E5AB6F7DB2E2CEEF136AC23D5DD2BF031700AEC232F6C6B1C785B4305C123B37AB")]
